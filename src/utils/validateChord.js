39220710f8b2bc462020dc51e70334f39c93766e
Fernando Fleury <fernandofleuryg@gmail.com>
2018-04-23 13:43:44 +0200

    Add validateChord spec

--- src/utils/validateChord.js
+++ src/utils/validateChord.js
@@ -1,13 +1,13 @@
 import { join, concat, toUpper, equals, replace, compose } from 'ramda';
 
-export const normalizedChord = compose(toUpper, join(''));
+export const normalizeChord = compose(toUpper, join(''));
 
-export const normalizedAnswer = compose(replace(/[^A-Z|#]/g, ''), toUpper);
+export const normalizeAnswer = compose(replace(/[^A-Z|#]/g, ''), toUpper);
 
 const isValid = (chord, answer) =>
   equals(
-    normalizedChord(chord.notes),
-    compose(concat(chord.root), normalizedAnswer)(answer)
+    normalizeChord(chord.notes),
+    compose(concat(chord.root), normalizeAnswer)(answer)
   );
 
 export default isValid;
---
+++ src/utils/validateChord.spec.js
@@ -0,0 +1,45 @@
+import isValid, { normalizeChord, normalizeAnswer } from './validateChord';
+
+describe('validateChord utils spec', () => {
+  describe('normalizeChord', () => {
+    it('joins the array and converts to toUpper', () => {
+      const data = ['c', 'e', 'g'];
+      const expectedData = 'CEG';
+
+      expect(normalizeChord(data)).toBe(expectedData);
+    });
+  });
+
+  describe('normalizeAnswer', () => {
+    it('sanitizes the string and converts to toUpper', () => {
+      const data = 'C  e, G 123.....,,,,,';
+      const expectedData = 'CEG';
+
+      expect(normalizeAnswer(data)).toBe(expectedData);
+    });
+  });
+
+  describe('isValid', () => {
+    it('returns true when the provided string is equal to current chord', () => {
+      const data = '  e, G';
+      const chord = {
+        root: 'C',
+        notes: ['C', 'E', 'G'],
+      };
+      const expectedData = true;
+
+      expect(isValid(chord, data)).toBeTruthy();
+    });
+
+    it('returns false when the provided string is equal to current chord', () => {
+      const data = 'e, G#';
+      const chord = {
+        root: 'C',
+        notes: ['C', 'E', 'G'],
+      };
+      const expectedData = false;
+
+      expect(isValid(chord, data)).toBeFalsy();
+    });
+  });
+});
